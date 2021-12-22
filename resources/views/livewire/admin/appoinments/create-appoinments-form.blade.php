0afa5b677f5fd8a8aaff6b520dd4fdcb9cc0135e
Shishir Bhuiyan <60442502+ShishirBhuiyan@users.noreply.github.com>
2021-12-23 02:08:35 +0600

    .

--- resources/views/livewire/admin/appoinments/create-appoinments-form.blade.php
+++ resources/views/livewire/admin/appoinments/create-appoinments-form.blade.php
@@ -1,3 +1,55 @@
 <div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+                        <li class="breadcrumb-item active"><a href="">Appointments</a></li>
+                        <li class="breadcrumb-item active">Create</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <form wire:submit.prevent="createAppointment" autocomplete="off">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Add New Appointment</h3>
+                            </div>
+                            <div class="card-body">
+                                <div class="row">
+                                    <!-- Date Picker -->
+                                    <div class="col-md-4">
+                                    </div>
+
+                                    <div class="col-md-4">
+                                        <!-- Time Picker -->
+                                    </div>
+
+                                    <div class="col-md-4">
+                                        <!-- Color Picker -->
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                <button type="button" class="btn btn-secondary"><i class="fa fa-times mr-1"></i> Cancel</button>
+                                <button id="submit" type="submit" class="btn btn-primary"><i class="fa fa-save mr-1"></i> Save</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
+  
+    
+@endpush
