c3f45604fd51ba2b23c8792c117f5bf9e50be371
Shishir Bhuiyan <60442502+ShishirBhuiyan@users.noreply.github.com>
2021-12-24 21:34:25 +0600

    get InputEvent as a Livewire dispatchEvent

--- resources/views/components/date-picker.blade.php
+++ resources/views/components/date-picker.blade.php
@@ -1,15 +1,16 @@
 
     <!-- Life is available only in the present moment. - Thich Nhat Hanh -->
     <input {{ $attributes }} type="text" class="form-control datetimepicker-input" id="datetimepicker5" data-toggle="datetimepicker" data-target="#datetimepicker5" 
+    onchange="this.dispatchEvent(new InputEvent('input'))"
     />
 
 @push('scripts')
-<script type="text/javascript">
-    $(function () {
-        $('#datetimepicker5').datetimepicker({
-            format: 'L'
+    <script type="text/javascript">
+        $(function () {
+            $('#datetimepicker5').datetimepicker({
+                format: 'L'
+            });
         });
-    });
-</script>
+    </script>
 @endpush
 
